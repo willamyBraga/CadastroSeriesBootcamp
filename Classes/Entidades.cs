23a6aea7eb7e97a473da67a4e3dde15ed3036898
willamybraga <willamybraga@yahoo.com>
2022-05-03 10:11:14 -0300

    Adicionar arquivos de projeto.

---
+++ Classes/Entidades.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroSeries
+{
+    public abstract class Entidades
+    {
+        public int Id { get;  set; }
+    }
+}
